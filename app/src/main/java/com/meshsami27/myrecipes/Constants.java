c25d75829bfd10547c3dbbb24f14a142b466d977
sammwaura <meshsami27@gmail.com>
2018-06-21 11:49:49 +0300

    modify ui and Constants

--- app/src/main/java/com/meshsami27/myrecipes/Constants.java
+++ app/src/main/java/com/meshsami27/myrecipes/Constants.java
@@ -5,6 +5,7 @@ public class Constants {
     public static final String CATEGORYDB_QUERY_PARAMS  = "c";
     public static final String RECIPEDB_BASE_URL = "https://www.themealdb.com/api/json/v1/1/lookup.php";
     public static final String RECIPEDB_QUERY_PARAMS = "i";
+    public static final String PREERENCES_CATEGORY_KEY="category";
 
 
 }
--- app/src/main/java/com/meshsami27/myrecipes/ui/MainActivity.java
+++ app/src/main/java/com/meshsami27/myrecipes/ui/MainActivity.java
@@ -1,46 +1,46 @@
-//package com.meshsami27.myrecipes.ui;
-//
-//import android.content.Intent;
-//import android.support.v7.app.AppCompatActivity;
-//import android.os.Bundle;
-//import android.view.View;
-//import android.widget.Button;
-//import android.widget.EditText;
-//
-//
-//import com.meshsami27.myrecipes.R;
-//
-//import butterknife.BindView;
-//import butterknife.ButterKnife;
-//
-//public class MainActivity extends AppCompatActivity  {
-//
-//
-//    @BindView(R.id.categoryEditText)
-//    EditText mCategoryEditText;
-//    @BindView(R.id.findCategoryButton)
-//    Button mFindCategoryButton;
-//
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-//        ButterKnife.bind(this);
-//
-//
-//        mFindCategoryButton.setOnClickListener(new View.OnClickListener() {
-//
-//
-//            @Override
-//            public void onClick(View v) {
-//                String category = mCategoryEditText.getText().toString();
-//                Intent intent = new Intent(MainActivity.this, MealListActivity.class);
-//                intent.putExtra("category", category);
-//                startActivity(intent);
-//            }
-//        });
-//
-//
-//    }
-//}
+package com.meshsami27.myrecipes.ui;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+import com.meshsami27.myrecipes.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity  {
+
+
+    @BindView(R.id.categoryEditText)
+    EditText mCategoryEditText;
+    @BindView(R.id.findCategoryButton)
+    Button mFindCategoryButton;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+
+        mFindCategoryButton.setOnClickListener(new View.OnClickListener() {
+
+
+            @Override
+            public void onClick(View v) {
+                String category = mCategoryEditText.getText().toString();
+                Intent intent = new Intent(MainActivity.this, MealListActivity.class);
+                intent.putExtra("category", category);
+                startActivity(intent);
+            }
+        });
+
+
+    }
+}
