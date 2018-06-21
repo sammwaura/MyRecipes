31161214d7c7e7b8723e917ece58676a749ad936
sammwaura <meshsami27@gmail.com>
2018-06-21 11:35:17 +0300

    modify ui

--- app/src/main/java/com/meshsami27/myrecipes/ui/DetailRecipeActivity.java
+++ app/src/main/java/com/meshsami27/myrecipes/ui/DetailRecipeActivity.java
@@ -1,88 +1,88 @@
-//package com.meshsami27.myrecipes.ui;
-//
-//import android.content.Intent;
-//import android.os.Parcel;
-//import android.support.v7.app.AppCompatActivity;
-//import android.os.Bundle;
-//import android.view.View;
-//import android.widget.ImageView;
-//import android.widget.TextView;
-//
-//import com.meshsami27.myrecipes.R;
-//import com.meshsami27.myrecipes.adapters.MyRecipesListAdapter;
-//import com.meshsami27.myrecipes.models.Recipe;
-//import com.meshsami27.myrecipes.services.MealDBService;
-//import com.squareup.picasso.Picasso;
-//
-//import org.parceler.Parcels;
-//
-//import java.io.IOException;
-//
-//
-//import butterknife.BindView;
-//import okhttp3.Call;
-//import okhttp3.Callback;
-//import okhttp3.Response;
-//
-//public class DetailRecipeActivity extends AppCompatActivity {
-//    @BindView(R.id.mealNameTextView)
-//    TextView mMealNameTextView;
-//    @BindView(R.id.categoryTextView)
-//    TextView mCategoryTextView;
-//    @BindView(R.id.instructionsTextView)
-//    TextView mInstrutionsTextView;
-//    @BindView(R.id.imageView)
-//    ImageView mImageView;
-//
-//    private MyRecipesListAdapter mAdapter;
-//
-//    Recipe mRecipe;
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//
-//        setContentView(R.layout.activity_detail_recipe);
-//
-//        Intent intent = getIntent();
-//
-//        String id = intent.getStringExtra("id");
-//
-//        getRecipe("id");
-//    }
-//
-//
-//
-//    private void getRecipe(String id) {
-//
-//
-//        final MealDBService mealDBService = new MealDBService();
-//
-//        mealDBService.filterMealRecipe(id, new Callback() {
-//
-//            @Override
-//
-//            public void onFailure(Call call, IOException e) {
-//                e.printStackTrace();
-//            }
-//
-//            @Override
-//            public void onResponse(Call call, Response response) throws IOException {
-//
-//                mRecipe = (Recipe) mealDBService.processRecipeResults(response);
-//
-//                DetailRecipeActivity.this.runOnUiThread(new Runnable() {
-//
-//                    @Override
-//                    public void run() {
-//                        mMealNameTextView.setText(mRecipe.getStrMeal());
-//                        mCategoryTextView.setText(mRecipe.getStrCategory());
-//                        mInstrutionsTextView.setText(mRecipe.getStrInstructions());
-//                        Picasso.with(DetailRecipeActivity.this).load(mRecipe.getStrMealThumb()).into(mImageView);
-//
-//                    }
-//                });
-//            }
-//        });
-//    }
-//}
+package com.meshsami27.myrecipes.ui;
+
+import android.content.Intent;
+import android.os.Parcel;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.meshsami27.myrecipes.R;
+import com.meshsami27.myrecipes.adapters.MyRecipesListAdapter;
+import com.meshsami27.myrecipes.models.Recipe;
+import com.meshsami27.myrecipes.services.MealDBService;
+import com.squareup.picasso.Picasso;
+
+import org.parceler.Parcels;
+
+import java.io.IOException;
+
+
+import butterknife.BindView;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
+
+public class DetailRecipeActivity extends AppCompatActivity {
+    @BindView(R.id.mealNameTextView)
+    TextView mMealNameTextView;
+    @BindView(R.id.categoryTextView)
+    TextView mCategoryTextView;
+    @BindView(R.id.instructionsTextView)
+    TextView mInstrutionsTextView;
+    @BindView(R.id.imageView)
+    ImageView mImageView;
+
+    private MyRecipesListAdapter mAdapter;
+
+    Recipe mRecipe;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_detail_recipe);
+
+        Intent intent = getIntent();
+
+        String id = intent.getStringExtra("id");
+
+        getRecipe("id");
+    }
+
+
+
+    private void getRecipe(String id) {
+
+
+        final MealDBService mealDBService = new MealDBService();
+
+        mealDBService.filterMealRecipe(id, new Callback() {
+
+            @Override
+
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+
+                mRecipe = (Recipe) mealDBService.processRecipeResults(response);
+
+                DetailRecipeActivity.this.runOnUiThread(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        mMealNameTextView.setText(mRecipe.getStrMeal());
+                        mCategoryTextView.setText(mRecipe.getStrCategory());
+                        mInstrutionsTextView.setText(mRecipe.getStrInstructions());
+                        Picasso.with(DetailRecipeActivity.this).load(mRecipe.getStrMealThumb()).into(mImageView);
+
+                    }
+                });
+            }
+        });
+    }
+}
--- app/src/main/java/com/meshsami27/myrecipes/ui/MealListActivity.java
+++ app/src/main/java/com/meshsami27/myrecipes/ui/MealListActivity.java
@@ -1,106 +1,106 @@
-//package com.meshsami27.myrecipes.ui;
-//
-//import android.content.Intent;
-//import android.os.Bundle;
-//import android.support.v7.app.AppCompatActivity;
-//import android.support.v7.widget.RecyclerView;
-//import android.support.v7.widget.LinearLayoutManager;
-//
-//
-//import com.meshsami27.myrecipes.R;
-//import com.meshsami27.myrecipes.adapters.MyRecipesListAdapter;
-//import com.meshsami27.myrecipes.models.Meal;
-//import com.meshsami27.myrecipes.models.Recipe;
-//import com.meshsami27.myrecipes.services.MealDBService;
-//import com.squareup.picasso.Picasso;
-//
-//
-//import org.parceler.Parcels;
-//
-//import okhttp3.Call;
-//import okhttp3.Callback;
-//import java.io.IOException;
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//import okhttp3.Response;
-//import butterknife.BindView;
-//import butterknife.ButterKnife;
-//
-//public class MealListActivity extends AppCompatActivity {
-//
-//    public static final String TAG = MealListActivity.class.getSimpleName();
-//
-//    @BindView(R.id.recyclerView)
-//    RecyclerView mRecyclerView;
-//
-//    private MyRecipesListAdapter mAdapter;
-//
-//    private List<Meal> mMeals = new ArrayList<>();
-//
-//    Recipe mRecipe;
-//
-//
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//
-//        setContentView(R.layout.activity_meal);
-//
-//        ButterKnife.bind(this);
-//
-//        Intent intent = getIntent();
-//
-//        String category = intent.getStringExtra("category");
-//
-//        getMeals(category);
-//
-//
-//
-//    }
-//
-//    private void getMeals(String meals) {
-//
-//        final MealDBService mealDBService = new MealDBService();
-//
-//        mealDBService.filterCategory(meals, new Callback() {
-//
-//            @Override
-//
-//            public void onFailure(Call call, IOException e) {
-//                e.printStackTrace();
-//            }
-//
-//            @Override
-//            public void onResponse(Call call, Response response) throws IOException {
-//
-//                mMeals = mealDBService.processMealResults(response);
-//
-//                MealListActivity.this.runOnUiThread(new Runnable() {
-//
-//                    @Override
-//                    public void run() {
-//                        mAdapter = new MyRecipesListAdapter(getApplicationContext(), mMeals, mRecipe);
-//
-//                        mRecyclerView.setAdapter(mAdapter);
-//
-//                        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(MealListActivity.this);
-//
-//                        mRecyclerView.setLayoutManager(layoutManager);
-//
-//                        mRecyclerView.setHasFixedSize(true);
-//
-//                    }
-//                });
-//
-//            }
-//
-//        });
-//
-//    }
-//
-//}
+package com.meshsami27.myrecipes.ui;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.LinearLayoutManager;
+
+
+import com.meshsami27.myrecipes.R;
+import com.meshsami27.myrecipes.adapters.MyRecipesListAdapter;
+import com.meshsami27.myrecipes.models.Meal;
+import com.meshsami27.myrecipes.models.Recipe;
+import com.meshsami27.myrecipes.services.MealDBService;
+import com.squareup.picasso.Picasso;
+
+
+import org.parceler.Parcels;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import okhttp3.Response;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MealListActivity extends AppCompatActivity {
+
+    public static final String TAG = MealListActivity.class.getSimpleName();
+
+    @BindView(R.id.recyclerView)
+    RecyclerView mRecyclerView;
+
+    private MyRecipesListAdapter mAdapter;
+
+    private List<Meal> mMeals = new ArrayList<>();
+
+    Recipe mRecipe;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_meal);
+
+        ButterKnife.bind(this);
+
+        Intent intent = getIntent();
+
+        String category = intent.getStringExtra("category");
+
+        getMeals(category);
+
+
+
+    }
+
+    private void getMeals(String meals) {
+
+        final MealDBService mealDBService = new MealDBService();
+
+        mealDBService.filterCategory(meals, new Callback() {
+
+            @Override
+
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+
+                mMeals = mealDBService.processMealResults(response);
+
+                MealListActivity.this.runOnUiThread(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        mAdapter = new MyRecipesListAdapter(getApplicationContext(), mMeals, mRecipe);
+
+                        mRecyclerView.setAdapter(mAdapter);
+
+                        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(MealListActivity.this);
+
+                        mRecyclerView.setLayoutManager(layoutManager);
+
+                        mRecyclerView.setHasFixedSize(true);
+
+                    }
+                });
+
+            }
+
+        });
+
+    }
+
+}
 
 
 
