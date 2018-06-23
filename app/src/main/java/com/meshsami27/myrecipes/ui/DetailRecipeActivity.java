83f36935648743705e8bcd1fb57e111bc690a9ce
sammwaura <meshsami27@gmail.com>
2018-06-23 15:05:04 +0300

    modify ui

--- app/src/main/java/com/meshsami27/myrecipes/ui/DetailRecipeActivity.java
+++ app/src/main/java/com/meshsami27/myrecipes/ui/DetailRecipeActivity.java
@@ -31,7 +31,47 @@ public class DetailRecipeActivity extends AppCompatActivity {
     @BindView(R.id.instructionsTextView)
     TextView mInstrutionsTextView;
     @BindView(R.id.imageView)
-    ImageView mImageView;
+   ImageView mImageView;
+//    @BindView(R.id.ingredient1TextView)
+//    TextView mIngredient1View;
+//    @BindView(R.id.ingredient2TextView)
+//    TextView mIngredient2View;
+//    @BindView(R.id.ingredient3TextView)
+//    TextView mIngredient3View;
+//    @BindView(R.id.ingredient4TextView)
+//    TextView mIngredient4View;
+//    @BindView(R.id.ingredient5TextView)
+//    TextView mIngredient5View;
+//    @BindView(R.id.ingredient6TextView)
+//    TextView mIngredient6View;
+//    @BindView(R.id.ingredient7TextView)
+//    TextView mIngredient7View;
+//    @BindView(R.id.ingredient8TextView)
+//    TextView mIngredient8View;
+//    @BindView(R.id.ingredient9TextView)
+//    TextView mIngredient9View;
+//    @BindView(R.id.ingredient10TextView)
+//    TextView mIngredient10View;
+//    @BindView(R.id.ingredient11TextView)
+//    TextView mIngredient11View;
+//    @BindView(R.id.ingredient12TextView)
+//    TextView mIngredient12View;
+//    @BindView(R.id.ingredient13TextView)
+//    TextView mIngredient13View;
+//    @BindView(R.id.ingredient14TextView)
+//    TextView mIngredient14View;
+//    @BindView(R.id.ingredient15TextView)
+//    TextView mIngredient15View;
+//    @BindView(R.id.ingredient16TextView)
+//    TextView mIngredient16View;
+//    @BindView(R.id.ingredient17TextView)
+//    TextView mIngredient17View;
+//    @BindView(R.id.ingredient18TextView)
+//    TextView mIngredient18View;
+//    @BindView(R.id.ingredient19TextView)
+//    TextView mIngredient19View;
+//    @BindView(R.id.ingredient20TextView)
+//    TextView mIngredient20View;
 
     private MyRecipesListAdapter mAdapter;
 
@@ -75,6 +115,26 @@ public class DetailRecipeActivity extends AppCompatActivity {
                     mMealNameTextView.setText(mRecipe.getStrMeal());
                     mCategoryTextView.setText(mRecipe.getStrCategory());
                     mInstrutionsTextView.setText(mRecipe.getStrInstructions());
+//                    mIngredient1View.setText(mRecipe.getStrIngredient1());
+//                    mIngredient2View.setText(mRecipe.getStrIngredient2());
+//                    mIngredient3View.setText(mRecipe.getStrIngredient3());
+//                    mIngredient4View.setText(mRecipe.getStrIngredient4());
+//                    mIngredient5View.setText(mRecipe.getStrIngredient5());
+//                    mIngredient6View.setText(mRecipe.getStrIngredient6());
+//                    mIngredient7View.setText(mRecipe.getStrIngredient7());
+//                    mIngredient8View.setText(mRecipe.getStrIngredient8());
+//                    mIngredient9View.setText(mRecipe.getStrIngredient9());
+//                    mIngredient10View.setText(mRecipe.getStrIngredient10());
+//                    mIngredient11View.setText(mRecipe.getStrIngredient11());
+//                    mIngredient12View.setText(mRecipe.getStrIngredient12());
+//                    mIngredient13View.setText(mRecipe.getStrIngredient13());
+//                    mIngredient14View.setText(mRecipe.getStrIngredient14());
+//                    mIngredient15View.setText(mRecipe.getStrIngredient15());
+////                    mIngredient16View.setText(mRecipe.getStrIngredient16());
+////                    mIngredient17View.setText(mRecipe.getStrIngredient17());
+////                    mIngredient18View.setText(mRecipe.getStrIngredient18());
+////                    mIngredient19View.setText(mRecipe.getStrIngredient19());
+////                    mIngredient20View.setText(mRecipe.getStrIngredient20());
                     Picasso.with(DetailRecipeActivity.this).load(mRecipe.getStrMealThumb()).into(mImageView);
                 });
             }
