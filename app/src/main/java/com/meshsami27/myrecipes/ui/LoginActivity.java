c52d8d0eed973127de773411623b3abc1c7da6de
sammwaura <meshsami27@gmail.com>
2018-06-28 09:13:35 +0300

    modify LoginActivity

--- app/src/main/java/com/meshsami27/myrecipes/ui/LoginActivity.java
+++ app/src/main/java/com/meshsami27/myrecipes/ui/LoginActivity.java
@@ -4,9 +4,12 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.meshsami27.myrecipes.R;
 
 import butterknife.BindView;
@@ -14,8 +17,18 @@ import butterknife.ButterKnife;
 
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+    public static final String TAG = LoginActivity.class.getSimpleName();
+
+    @BindView(R.id.passwordLoginButton)
+    Button mPasswordLoginButton;
+    @BindView(R.id.emailEditText)
+    EditText mEmailEditText;
+    @BindView(R.id.passwordEditText)
+    EditText mPasswordEditText;
     @BindView(R.id.registerTextView)
     TextView mRegisterTextView;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +36,9 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         setContentView(R.layout.activity_login);
 
         ButterKnife.bind(this);
+        mPasswordLoginButton.setOnClickListener(this);
         mRegisterTextView.setOnClickListener(this);
+        mAuth = FirebaseAuth.getInstance();
     }
 
     @Override
@@ -32,6 +47,22 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             Intent intent = new Intent(LoginActivity.this, CreateAccountActivity.class);
             startActivity(intent);
             finish();
+        }
+        if (view == mPasswordLoginButton){
+            loginWithPassword();
+        }
+    }
+
+    private void loginWithPassword(){
+        String email = mEmailEditText.getText().toString().trim();
+        String password = mPasswordLoginButton.getText().toString().trim();
+        if (email.equals("")){
+            mEmailEditText.setError("Please enter your email");
+            return;
+        }
+        if (password.equals("")){
+            mPasswordEditText.setError("Password cannot be blank");
+            return;
         }
     }
 }
